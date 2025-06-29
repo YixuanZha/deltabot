b9561c26385d60fd848f6ce88044ce72c3c68b10
zyx <2511305125@qq.com>
2025-06-29 19:13:17 +0100

    Refactor CaptureCameraFeed class to improve camera initialization and frame processing

--- scripts/include/CaptureCameraFeed.h
+++ scripts/include/CaptureCameraFeed.h
@@ -2,18 +2,33 @@
 #define CAPTURECAMERAFEED_H
 
 #include <opencv2/opencv.hpp>
-#include <iostream>
+#include <vector>
 #include <string>
+#include <chrono>
+#include <sstream>
 
 class CaptureCameraFeed
 {
 public:
-    CaptureCameraFeed(int camera_id);
+    CaptureCameraFeed(int device_index, int capture_width, int capture_height, int framerate);
     void run();
 
 private:
-    int camera_id_;
+    int frame_width;
+    int frame_height;
+    int frame_count = 0;
+    double fps_to_display;
+    cv::Mat frame;
+
+    std::chrono::time_point<std::chrono::steady_clock> start_time;
+
     cv::VideoCapture cap_;
+
+    std::string GetGstreamPipeline(int device_index, int width, int height, int fps);
+
+    void CalculateFPS();
+
+    void PrintOnScreen();
 
     cv::Mat preprocess(const cv::Mat &frame);
     
--- scripts/src/CaptureCameraFeed.cpp
+++ scripts/src/CaptureCameraFeed.cpp
@@ -1,40 +1,81 @@
 #include "CaptureCameraFeed.h"
 
-CaptureCameraFeed::CaptureCameraFeed(int camera_id)
-    : camera_id_(camera_id)
+CaptureCameraFeed::CaptureCameraFeed(int device_index, int capture_width, int capture_height, int framerate)
+    : frame_width(capture_width), frame_height(capture_height),frame_count(0), fps_to_display(0.0)
 {
-    cap_.open(camera_id_);
-    if (!cap_.isOpened())
+    std::string pipeline = GetGstreamPipeline(device_index,capture_width,capture_height,framerate);
+    std::cout << "Using GStreamer pipeline:" << pipeline << std::endl;
+
+    cap_.open(pipeline,cv::CAP_GSTREAMER);
+
+    if(!cap_.isOpened())
     {
-        std::cerr << "ERROR: Can not open camera:" << camera_id_ << std::endl;
-        throw std::runtime_error("Failed to open the camera");
+        std::cerr << "ERROR: Failed to opened camera with pipeline." << std::endl;
+        throw std::runtime_error("Failed to open camera");
     }
 }
 
 void CaptureCameraFeed::run()
 {
-    cv::Mat frame;
+    start_time = std::chrono::steady_clock::now();
     while (true)
     {
-        cap_ >> frame;
-        if (frame.empty())
+        if(!cap_.read(frame))
         {
-            std::cerr << "ERROR: Null framed captured" << std::endl;
-            break;
+            std::cerr << ("ERROR: frame could not be captured from the camera") << std::endl;
+            break;  
         }
 
-        cv::flip(frame,frame,0); // flip the frame
-        cv::Mat DisplayFrame;
-        int DisplayWidth = 1050;
-        int DisplayHight = static_cast<int>(static_cast<double>(frame.rows * DisplayWidth) / frame.cols);
-        cv::resize(frame, DisplayFrame, cv::Size(DisplayWidth, DisplayHight));
+        cv::flip(frame, frame, 0); // flip the frame
 
-        cv::imshow("Camera Feed", DisplayFrame);
+        frame_count++;
+        CalculateFPS();
+        PrintOnScreen();
+
+        cv::imshow("Camera Feed", frame);
         if (cv::waitKey(1) == 27)
         {
             break;
         }
     }
+}
+
+std::string CaptureCameraFeed::GetGstreamPipeline(int device_index, int width, int height, int fps)
+{
+     return "v4l2src device=/dev/video" + std::to_string(device_index) +
+           " io-mode=4 ! video/x-raw, width=" + std::to_string(width) +
+           ", height=" + std::to_string(height) + ", framerate=" +
+           std::to_string(fps) + "/1 ! videoconvert ! " +
+           "video/x-raw,format=BGR ! appsink";
+}
+
+void CaptureCameraFeed::CalculateFPS()
+{
+    auto end_time = std::chrono::steady_clock::now();
+    auto elapsed_time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
+
+    if(elapsed_time_ms >= 1000)
+    {
+        fps_to_display = frame_count * 1000.0 / elapsed_time_ms;
+
+        start_time = std::chrono::steady_clock::now();
+        frame_count = 0;
+    }
+}
+
+void CaptureCameraFeed::PrintOnScreen()
+{
+    std::stringstream ss;
+    ss << std::fixed << std::setprecision(2) << fps_to_display;
+    std::string fps_text = "FPS: " + ss.str();
+
+     cv::putText(frame,
+                    fps_text,             
+                    cv::Point(10, 30),     
+                    cv::FONT_HERSHEY_SIMPLEX, 
+                    1.0,                   
+                    cv::Scalar(0, 255, 0),  
+                    2);  
 }
 
 cv::Mat CaptureCameraFeed::preprocess(const cv::Mat &frame)
