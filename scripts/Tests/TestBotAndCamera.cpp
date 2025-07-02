b9defefeaad412d57098928c6d402e5b6007fb88
zyx <2511305125@qq.com>
2025-07-02 12:38:08 +0100

    Refactor getch function to streamline character input handling and remove unused raw mode functions

--- scripts/Tests/TestBotAndCamera.cpp
+++ scripts/Tests/TestBotAndCamera.cpp
@@ -9,43 +9,25 @@ using namespace std;
 // A simple test uses the keyboard to control the robot's forward, backward and steering, and to regulate speed (using polling)
 // Test the camera at the same time
 
-struct termios orig_termios;
-
-void DisableRawMode()
+/**
+ * Reads individual character input directly from the keyboard without the need for a carriage return.
+ * @return {char}  : input character
+ */
+char getch()
 {
-    tcsetattr(STDIN_FILENO,TCSANOW, &orig_termios);
+    struct termios oldt, newt; // Save settings of old and new terminal
+    char ch;
+    tcgetattr(STDIN_FILENO, &oldt); // Get current terminal settings
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);        // Close canonical mode and echo (Do not need to press enter and the character will not be displayed on the screen)
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt); // Apply new settings
+    ch = getchar();                          // Get one character
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // Restore original settings
+    return ch;
 }
-
-void EnableRawMode()
-{
-    tcgetattr(STDIN_FILENO,&orig_termios);
-    atexit(DisableRawMode);
-
-    struct termios raw = orig_termios;
-    raw.c_cflag &= ~(ECHO | ICANON);
-    tcsetattr(STDIN_FILENO, TCSANOW, &raw);
-}
-
-// /**
-//  * Reads individual character input directly from the keyboard without the need for a carriage return.
-//  * @return {char}  : input character
-//  */
-// char getch()
-// {
-//     struct termios oldt, newt; // Save settings of old and new terminal
-//     char ch;
-//     tcgetattr(STDIN_FILENO, &oldt); // Get current terminal settings
-//     newt = oldt;
-//     newt.c_lflag &= ~(ICANON | ECHO);        // Close canonical mode and echo (Do not need to press enter and the character will not be displayed on the screen)
-//     tcsetattr(STDIN_FILENO, TCSANOW, &newt); // Apply new settings
-//     ch = getchar();                          // Get one character
-//     tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // Restore original settings
-//     return ch;
-// }
 
 int main(int argc, char *argv[])
 {
-    EnableRawMode();
     DeltaBot deltabot;
     bool running = true;
 
@@ -56,26 +38,9 @@ int main(int argc, char *argv[])
 
     while (running)
     {
-        // Use select to wait for input from stdin with a timeout of 10 milliseconds
-        // This allows the program to continue running without blocking on input
-        // If there is input, it will read the character and execute the corresponding command
-        // If there is no input, it will continue to the next iteration of the loop
-        // This is a simple way to implement non-blocking input handling in a console application
-        fd_set fd;
-        FD_ZERO(&fd);
-        FD_SET(STDIN_FILENO, &fd); // Set the file descripter of stdin to 1
-
-        struct timeval timeout;
-        timeout.tv_sec = 0;
-        timeout.tv_usec = 10000;
-
-        int activity = select(STDIN_FILENO + 1, &fd, NULL, NULL, &timeout); // Wait for input on stdin with a timeout of 10 milliseconds
-
-        if (activity > 0)
+        if (true)
         {
-            // char cmd = getch();
-            char cmd;
-            read(STDIN_FILENO,&cmd,1);
+            char cmd = getch();
             switch (cmd)
             {
             case 'w':
