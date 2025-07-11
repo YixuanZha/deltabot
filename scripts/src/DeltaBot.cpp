e5a0d247dc796ce2741db1ec56074da945771ddb
zyx <2511305125@qq.com>
2025-07-11 02:03:13 +0100

    Refactor motor control methods to use SetMotorSpeed for consistency

--- scripts/src/DeltaBot.cpp
+++ scripts/src/DeltaBot.cpp
@@ -9,26 +9,22 @@ DeltaBot::DeltaBot()
 
 void DeltaBot::Forward(float speed) // Move the robot forward
 {
-    leftMotor.ChangeLeftSpeed(speed);
-    rightMotor.ChangeRightSpeed(-speed);
+    SetMotorSpeed(speed, speed);
 }
 
 void DeltaBot::Backward(float speed) // Move the robot backward
 {
-    leftMotor.ChangeLeftSpeed(-speed);
-    rightMotor.ChangeRightSpeed(speed);
+    SetMotorSpeed(-speed, -speed);
 }
 
 void DeltaBot::TurnLeft(float speed) // Turn the robot to the left
 {
-    leftMotor.ChangeLeftSpeed(speed / 2.0f);
-    rightMotor.ChangeRightSpeed(-speed);
+    SetMotorSpeed(speed / 2.0f, speed);
 }
 
 void DeltaBot::TurnRight(float speed) // Turn the robot to the right
 {
-    leftMotor.ChangeLeftSpeed(speed);
-    rightMotor.ChangeRightSpeed(-speed / 2.0f);
+    SetMotorSpeed(speed, speed / 2.0f);
 }
 
 void DeltaBot::Stop() // Stop the robot
@@ -45,19 +41,19 @@ void DeltaBot::SetMotorSpeed(float left_speed, float right_speed)
 
 void DeltaBot::StartCamera() // Start the camera feed
 {
-    if(camera_thread.joinable())
+    if (camera_thread.joinable())
     {
         std::cout << "Camera thread is already running" << std::endl;
         return;
     }
-    camera_thread = std::thread(&CaptureCameraFeed::run,&camera);
+    camera_thread = std::thread(&CaptureCameraFeed::run, &camera);
     std::cout << "Camera thread started" << std::endl;
 }
 
 void DeltaBot::StopCamera() // Stop the camera feed
 {
     camera.stop();
-    if(camera_thread.joinable())
+    if (camera_thread.joinable())
     {
         camera_thread.join();
         std::cout << "Camera thread joined" << std::endl;
